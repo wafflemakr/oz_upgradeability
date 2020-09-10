76baf30709be95f7a1eec1f001f2ab41650206e3
FELIPE <wafflemakr@gmail.com>
2020-09-10 16:05:57 -0500

    remove test from migration file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,6 @@
 const AdminUpgradeabilityProxy = artifacts.require("AdminUpgradeabilityProxy");
 const ProxyAdmin = artifacts.require("ProxyAdmin");
 const V1 = artifacts.require("V1");
-const V2 = artifacts.require("V2");
 
 module.exports = async function (deployer, network, accounts) {
   if (network === "test") return;
@@ -11,57 +10,10 @@ module.exports = async function (deployer, network, accounts) {
   const proxyAdmin = await deployer.deploy(ProxyAdmin);
 
   // DEPLOY PROXY
-  const proxy = await deployer.deploy(
+  await deployer.deploy(
     AdminUpgradeabilityProxy,
     impl.address,
     proxyAdmin.address,
     encoded
   );
-
-  // Logic contract
-  const v1 = await V1.at(proxy.address);
-
-  console.log("\nFetching admin variables...");
-  const admin = await proxyAdmin.getProxyAdmin(proxy.address);
-  console.log(admin, proxyAdmin.address);
-  const owner = await proxyAdmin.owner();
-  console.log(owner, accounts[0]);
-  const implementation = await proxyAdmin.getProxyImplementation(proxy.address);
-  console.log(implementation, impl.address);
-
-  console.log("\nTesting logic variable...");
-  let value = await v1.testVariable({ from: accounts[1] });
-  console.log(String(value), 10);
-
-  console.log("\nUpgrading implementation...");
-  impl = await V2.new();
-  await proxyAdmin.upgrade(proxy.address, impl.address);
-  const v2 = await V2.at(proxy.address);
-
-  console.log("\nFetching variables again...");
-  const admin2 = await proxyAdmin.getProxyAdmin(proxy.address);
-  console.log(admin2, proxyAdmin.address);
-  const owner2 = await proxyAdmin.owner();
-  console.log(owner2, accounts[0]);
-  const implementation2 = await proxyAdmin.getProxyImplementation(
-    proxy.address
-  );
-  console.log(implementation2, impl.address);
-
-  console.log("\nTesting that storage is intact...");
-  value = await v2.testVariable();
-  console.log(String(value), 10); // still needs to be 10
-
-  console.log("\nWhat if I change the variable directly in logic contract?");
-  const logic = await V2.at(impl.address);
-  await logic.setVariable(666);
-  value = await logic.testVariable();
-  console.log(String(value), 666);
-  value = await v2.testVariable();
-  console.log(String(value), 10);
-
-  console.log("\nUpdating Storage");
-  await v2.setVariable(55);
-  value = await v2.testVariable();
-  console.log(String(value), 55);
 };
